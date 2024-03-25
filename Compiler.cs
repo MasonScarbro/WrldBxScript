b013e594e2c13f64230424d55ad737f0a4b6513a
MasonScarbro <masonscarbropf@gmail.com>
2024-03-24 20:50:06 -0400

    Small update, just got the compiler started

--- Compiler.cs
+++ Compiler.cs
@@ -8,14 +8,15 @@ namespace WrldBxScript
 {
     class Compiler
     {
-
+        private int count = 0;
+        private string src;
         public void Compile(List<Stmt> statements)
         {
             try
             {
                 foreach (Stmt statement in statements)
                 {
-                    //Execute(statement);
+                    var stmt = Execute(statement);
                 }
             }
             catch (CompilerError error)
@@ -24,7 +25,46 @@ namespace WrldBxScript
             }
         }
 
+        private Stmt Execute(Stmt stmt)
+        {
+            if (stmt is Stmt.Var stmtv)
+            {
+                if (stmtv.type.lexeme.ToUpper() == "HEALTH")
+                {
+                    
+                }
+            }
+            if (stmt is Stmt.Starter stmtst)
+            {
+                foreach (Stmt.Block block in stmtst.body)
+                {
+                    src = "class " + ToParaCase(stmtst.type.lexeme) + "\n" + "{" + "\n\tpublic static void init() \n{";
+                    
+                    Execute(block);
+                }
+                // reset src and count at the end of executing a starter since it descends
+                count = 0;
+                src = "";
+            }
+            if (stmt is Stmt.Block stmtb)
+            {
+                foreach (Stmt stat in stmtb.statements)
+                {
+                    
+                    Execute(stat);
+                }
+            }
+            if (stmt is Stmt.Expression stmte)
+            {
+                return stmte;
+            }
+            return null;
+        }
 
-
+        
+        private string ToParaCase(string str)
+        {
+            return str.Substring(0, 1).ToUpper() + str.Substring(1).ToLower();
+        }
     }
 }
